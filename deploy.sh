6b6feb741778a3a4c239daed4feff6e04f109ddd
Jayson McIntosh <jayson.r.mcintosh@gmail.com>
2020-12-14 11:55:46 -0500

    Renaming component from hello world

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:jaysonmc/digitopia-slider.git master:gh-pages
+git push -f https://github.com/jaysonmc/digitopia-slider main:gh-pages
 
 cd -
